accab651dd9796e35991945eec7d13cdaca3dff8
Michael Himing <mhiming@gmail.com>
2019-08-12 17:29:54 +1000

    Little ASM DSL

---
+++ src/main/java/io/prplz/skypixel/asm/ASMUtils.java
@@ -0,0 +1,37 @@
+package io.prplz.skypixel.asm;
+
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.JumpInsnNode;
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+
+public class ASMUtils {
+
+    public static InsnList insnListOf(Object... args) {
+        InsnList list = new InsnList();
+        for (Object arg : args) {
+            if (arg instanceof AbstractInsnNode) {
+                list.add((AbstractInsnNode) arg);
+            } else if (arg instanceof InsnList) {
+                list.add((InsnList) arg);
+            } else if (arg instanceof Integer) {
+                list.add(new InsnNode((Integer) arg));
+            } else {
+                throw new IllegalArgumentException("Arguments must be AbstractInsnNode or InsnList");
+            }
+        }
+        return list;
+    }
+
+    public static InsnList ifTrue(Object... args) {
+        LabelNode label = new LabelNode();
+        return insnListOf(new JumpInsnNode(Opcodes.IFEQ, label), insnListOf(args), label);
+    }
+
+    public static MethodInsnNode invokeStatic(String owner, String name, String desc) {
+        return new MethodInsnNode(Opcodes.INVOKESTATIC, owner, name, desc, false);
+    }
+}
--- src/main/java/io/prplz/skypixel/asm/patcher/ItemStackPatcher.java
+++ src/main/java/io/prplz/skypixel/asm/patcher/ItemStackPatcher.java
@@ -3,13 +3,10 @@ package io.prplz.skypixel.asm.patcher;
 import io.prplz.skypixel.asm.hook.ItemStackHook;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.JumpInsnNode;
-import org.objectweb.asm.tree.LabelNode;
-import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+
+import static io.prplz.skypixel.asm.ASMUtils.*;
+import static org.objectweb.asm.Opcodes.RETURN;
 
 public class ItemStackPatcher extends ClassPatcher {
 
@@ -28,12 +25,9 @@ public class ItemStackPatcher extends ClassPatcher {
      */
     @PatchMethod(name = {"damageItem", "func_77972_a"}, desc = "(ILnet/minecraft/entity/EntityLivingBase;)V")
     public void patchDamageItem(MethodNode method) {
-        InsnList instructions = new InsnList();
-        instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "io/prplz/skypixel/asm/hook/ItemStackHook", "damageItem", "()Z", false));
-        LabelNode label = new LabelNode();
-        instructions.add(new JumpInsnNode(Opcodes.IFEQ, label));
-        instructions.add(new InsnNode(Opcodes.RETURN));
-        instructions.add(label);
-        method.instructions.insert(instructions);
+        method.instructions.insert(insnListOf(
+                invokeStatic("io/prplz/skypixel/asm/hook/ItemStackHook", "damageItem", "()Z"),
+                ifTrue(RETURN)
+        ));
     }
 }
