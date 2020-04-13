fb31b6553a05f5c92183d000ed037bd29ceb2ce7
Michael Himing <git@michaelhiming.com>
2020-04-13 15:23:08 +1000

    Add roman numerals up to 20

--- src/main/java/io/prplz/skypixel/utils/RomanNumerals.java
+++ src/main/java/io/prplz/skypixel/utils/RomanNumerals.java
@@ -2,7 +2,7 @@ package io.prplz.skypixel.utils;
 
 public class RomanNumerals {
 
-    private static final String[] numerals = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI"};
+    private static final String[] numerals = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX"};
 
     public static String get(int i) {
         if (i > 0 && i <= numerals.length) {
